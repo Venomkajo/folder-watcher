850eb5a3b7c6a8efb5246f10a7816c04f2d088ee
Venomkajo <kajetanrespondek@gmail.com>
2024-06-20 16:17:29 +0200

    added comments and reset function

--- modified-file-question.py
+++ modified-file-question.py
@@ -8,10 +8,12 @@ def main():
 
     files_in_current_directory = os.listdir(current_directory)
 
+    # print files currently in directory
     print('\nFiles in current directory:\n')
     for item in files_in_current_directory:
         print(item)
 
+    # if savefile.txt exists print it out and copy contents to saved_data
     if os.path.exists('savefile.txt'):
         print('\nSaved data:\n')
         with open('savefile.txt', 'r') as file:
@@ -19,25 +21,34 @@ def main():
             saved_data = data
             print(data)
 
+    # overwrite or create the savefile.txt
     with open('savefile.txt', 'w') as file:
         for item in files_in_current_directory:
             file.write(item + '\n')
 
+    # get the current_data
     with open('savefile.txt', 'r') as file:
         current_data = file.read()
 
+    # if contents were modified print out the missing content
     if (current_data == saved_data):
         print('\nNo changes to files.\n')
-    else:
+    elif saved_data:
         print('\nFiles were modified.\n')
         print('\nModified files:\n')
-        modified_files = get_modified_files(current_data, saved_data)
-        print(modified_files)
 
-    input('Program finished.')
+        modified_files = get_modified_files(current_data, saved_data)
+
+        for file in modified_files:
+            print(file)
+
+    # restart the program by typing R
+    if input('\nProgram finished.\n').upper() == 'R':
+        main()
 
     return 0
 
+# compare file lists and return the differences
 def get_modified_files(string1, string2):
     returnValues = []
 
