fbbbedc23c9a467d72743d182d02a87bcfd1c825
Venomkajo <kajetanrespondek@gmail.com>
2024-06-20 22:49:32 +0200

    added arguments for usage, overall code optimization

--- folder-watcher.py
+++ folder-watcher.py
@@ -1,36 +1,83 @@
 import os
 
-def main():
+SAVE_FILE = 'savefile.txt'
+TEMP_SAVE_FILE = 'tempsavefile.txt'
+
+def folder_watcher():
     current_data = ''
     saved_data = ''
+    argument = get_argument()
 
     current_directory = os.getcwd()
-
     files_in_current_directory = os.listdir(current_directory)
 
-    # print files currently in directory
+    # use functions based on the argument
+    # compare the files and immediately overwrite the savefile
+    if argument == 'A':
+        print_files(files_in_current_directory)
+        saved_data = read_savefile(SAVE_FILE)
+        create_savefile(files_in_current_directory, SAVE_FILE)
+        current_data = get_current(SAVE_FILE)
+        print_modified(current_data, saved_data)
+    # only overwrite the save file
+    elif argument == 'W':
+        create_savefile(files_in_current_directory, SAVE_FILE)
+    # read the file without overwriting the save file, using a temp file to guarantee formatting
+    elif argument == 'R':
+        print_files(files_in_current_directory)
+        saved_data = read_savefile()
+        create_savefile(files_in_current_directory, TEMP_SAVE_FILE)
+        current_data = get_current(TEMP_SAVE_FILE)
+        delete_file(TEMP_SAVE_FILE)
+        print_modified(current_data, saved_data)
+    else:
+        print('Error! Invalid argument.')
+
+    # restart the program by typing R
+    if input('\nProgram finished.\n').upper() == 'R':
+        folder_watcher()
+
+    return 0
+
+# get the argument
+def get_argument():
+    while True:
+        argument = input('\nArguments: W-Write, R-Read, A-All\n').upper()
+        if argument in ['W', 'R', 'A']:
+            return argument
+
+# print files currently in directory
+def print_files(files_list):
     print('\nFiles in current directory:\n')
-    for item in files_in_current_directory:
-        print(item)
+    for file in files_list:
+        print(file)
 
-    # if savefile.txt exists print it out and copy contents to saved_data
-    if os.path.exists('savefile.txt'):
+# if savefile.txt exists print it out and copy contents to saved_data
+def read_savefile(file_path):
+    if os.path.exists(file_path):
         print('\nSaved data:\n')
-        with open('savefile.txt', 'r') as file:
+        with open(file_path, 'r') as file:
             data = file.read()
-            saved_data = data
             print(data)
-
-    # overwrite or create the savefile.txt
-    with open('savefile.txt', 'w') as file:
-        for item in files_in_current_directory:
+            return data
+        
+# overwrite or create the savefile
+def create_savefile(files_list, file_path):
+    with open(file_path, 'w') as file:
+        for item in files_list:
             file.write(item + '\n')
 
-    # get the current_data
-    with open('savefile.txt', 'r') as file:
-        current_data = file.read()
+# delete the temporary file
+def delete_file(file_path):
+    os.remove(file_path)
 
-    # if contents were modified print out the missing content
+# get the current_data
+def get_current(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+    
+# if contents were modified print out the missing content
+def print_modified(current_data, saved_data):
     if (current_data == saved_data):
         print('\nNo changes to files.\n')
     elif saved_data:
@@ -41,12 +88,6 @@ def main():
 
         for file in modified_files:
             print(file)
-
-    # restart the program by typing R
-    if input('\nProgram finished.\n').upper() == 'R':
-        main()
-
-    return 0
 
 # compare file lists and return the differences
 def get_modified_files(string1, string2):
@@ -65,4 +106,4 @@ def get_modified_files(string1, string2):
 
     return returnValues
 
-main()
+folder_watcher()
